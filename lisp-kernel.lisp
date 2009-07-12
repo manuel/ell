88ac9bf3554f0e73adaf674a3992d9b956c21be2
Manuel Simoni <msimoni@gmail.com>
2009-07-12 20:34:24 +0200

    kernel language and ordered set example

---
+++ lisp-kernel.lisp
@@ -0,0 +1,76 @@
+(defpackage Lisp-kernel
+  (interface ()
+    ;; Built-in types and objects
+    (defclass <object>)
+    (defclass <type> <: <object>)
+    (defclass <class> <: <type>)
+    (defclass <mixin> <: <type>)
+    (defclass <form> <: <object>)
+    (defclass <symbol> <: <form>)
+    (defclass <list> <: <form>)
+    (defclass <boolean> <: <object>)
+    (defclass <true> <: <boolean>)
+    (defclass <false> <: <boolean>)
+    (defvar true)
+    (defvar false)
+    (defclass <null> <: <object>)
+    (defvar nil)
+    ;; Variables
+    (defsyntax defparameter (<symbol> <object>))
+    (defsyntax setq (<symbol> <object>))
+    (defun boundp (<symbol>) -> <boolean>)
+    ;; Functions
+    (defsyntax defun (<symbol> <function>))
+    (defsyntax function (<symbol>) -> <function>)
+    (defsyntax lambda (<signature> <form>) -> <function>)
+    (defun apply (<function> &rest args &keys key-args) -> <object>)
+    (defun fboundp (<symbol>) -> <boolean>)
+    (defun funcall (<function> args key-args))
+    ;; Control
+    (defsyntax if (<boolean> <form> &opt <form>) -> <object>)
+    (defsyntax progn (&body body) -> <object>)
+    (defsyntax unwind-protect (<form> &body cleanup-forms) -> <object>)
+    (defun call/ec (<function>) -> <object>)
+    ;; Syntax transformers
+    (defsyntax defsyntax (<signature> <function>))
+    (defsyntax let-syntax (transformer-bindings <form>) -> <object>)
+    (defsyntax let*-syntax (transformer-bindings <form>) -> <object>)
+    ;; Syntax objects
+    (defun datum->syntax-object (<environment> <form>) -> <form>)
+    (defun elt (<list> <number>) -> <form>)
+    (defun first (<list>) -> <form>)
+    (defun quasisyntax (<form>) -> <form>)
+    (defun rest (<list>) -> <form>)
+    (defun syntax (<form>) -> <form>)
+    ;; Classes
+    (defun make-class (<symbol>
+                       &key super-class mutable-super-class-p mutable-super-mixins-p mutable-slot-specs-p
+                       &rest slot-specs) -> <class>)
+    (defun set-method (<class> <symbol> <function>))
+    (defun set-slot-specs (<class> &rest slot-specs))
+    (defun set-super-class (<class> <class>))
+    ;; Mixins
+    (defun add-mixin (<type> <mixin>))
+    (defun make-mixin (<symbol> &key mutable-super-mixins-p &rest super-mixins) -> <mixin>)
+    (defun remove-mixin (<type> <mixin>))
+    ;; Objects
+    (defun call-applicable-method (<object> <symbol> &rest args &keys key-args) -> <object>)
+    (defun set-slot-value (<object> <symbol> <object>))
+    (defun slot-value (<object> <symbol>) -> <symbol>)
+    (defun type-of (<object>) -> <type>)
+    ;; Conditions
+    (defsyntax let-handler (handler-bindings <form>) -> <object>)
+    (signal (<condition>) -> <object>)
+    ;; Evaluation and Compilation
+    (defsyntax define-compiler-macro (<symbol> <function>))
+    (defun eval (<form>) -> <object>)
+    (defsyntax eval-when-compile (<form>))
+    ;; Packages
+    (deftype <package>)
+    (defsyntax defpackage (<symbol> <package-or-functor-application>))
+    (defsyntax interface (<signature> <declarations>) -> <package>)
+    (defsyntax implementation (<signature> <definitions>) -> <package>)
+    ;; UNIX
+    (defsyntax c (<string>) -> <object>)
+  )
+)
---
+++ ordered-set.lisp
@@ -0,0 +1,39 @@
+(defpackage Ordered-type
+  (interface ()
+    (deftype <t>)
+    (defun compare (<t> <t>) -> <int>)))
+
+(defpackage Set
+  (interface ()
+    (deftype <t>)
+    (deftype <elt>)
+    (defun make () -> <t>)
+    (defun add (<t> <elt>))))
+
+(defpackage Set-impl
+  (implementation ((Elt Ordered-type)) -> Set
+    (defclass <t> (elements <list> init: (list)))
+    (deftype <elt> Elt)
+    (defun make () -> <t> (make-t))
+    (defun add (<t> <elt>) (add (.elements t) elt)))) ; blah
+
+;; --
+
+(defpackage No-case-string
+  (implementation () -> Ordered-type
+    (deftype <t> <str>)
+    (defun compare ((s1 <str>) (s2 <str>)) -> <int>
+      (strcmp (tolower s1) (tolower s2)))))
+
+(defpackage No-case-string-set 
+  (Set-impl No-case-string))
+
+;; --
+
+(defpackage My
+  (implementation ()
+    (import S = No-case-string-set)
+    (let set = (S::make)
+      (S::add set "foo"))))
+
+;; Now I just need to figure out how to compile this.
