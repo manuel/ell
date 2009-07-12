9e90f839f0a7e2b3996520de0e0f62d12f637207
Manuel Simoni <msimoni@gmail.com>
2009-07-12 21:46:04 +0200

    new lambda list style

--- ordered-set.lisp
+++ ordered-set.lisp
@@ -1,28 +1,28 @@
 (defpackage Ordered-type
   (interface ()
     (deftype <t>)
-    (defun compare (<t> <t>) -> <int>)))
+    (defun compare (<t> <t> -> <int>))))
 
 (defpackage Set
   (interface ()
     (deftype <t>)
     (deftype <elt>)
-    (defun make () -> <t>)
+    (defun make (-> <t>))
     (defun add (<t> <elt>))))
 
 (defpackage Set-impl
-  (implementation ((Elt Ordered-type)) -> Set
+  (implementation ((Elt Ordered-type) -> Set)
     (defclass <t> (elements <list> init: (list)))
     (deftype <elt> Elt)
-    (defun make () -> <t> (make-t))
+    (defun make (-> <t>) (make-t))
     (defun add (<t> <elt>) (add (.elements t) elt)))) ; blah
 
 ;; --
 
 (defpackage No-case-string
-  (implementation () -> Ordered-type
+  (implementation (-> Ordered-type)
     (deftype <t> <str>)
-    (defun compare ((s1 <str>) (s2 <str>)) -> <int>
+    (defun compare ((s1 <str>) (s2 <str>) -> <int>)
       (strcmp (tolower s1) (tolower s2)))))
 
 (defpackage No-case-string-set 
