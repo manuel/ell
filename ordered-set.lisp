50d1582fcee494adacfd8bc869661b08d118f9c5
Manuel Simoni <msimoni@gmail.com>
2009-07-12 22:20:41 +0200

    abstract-set functor

--- ordered-set.lisp
+++ ordered-set.lisp
@@ -11,11 +11,15 @@
     (defun add (<t> <elt>))))
 
 (defpackage Set-impl
-  (implementation ((Elt Ordered-type) -> Set)
+  (implementation ((Elt Ordered-type))
     (defclass <t> (elements <list> init: (list)))
     (deftype <elt> Elt)
     (defun make (-> <t>) (make-t))
     (defun add (<t> <elt>) (add (.elements t) elt)))) ; blah
+
+(defpackage Abstract-set 
+  (implementation ((Elt Ordered-type) -> (Set <elt> = Elt::<t>))
+    Set-impl))
 
 ;; --
 
@@ -25,15 +29,17 @@
     (defun compare ((s1 <str>) (s2 <str>) -> <int>)
       (strcmp (tolower s1) (tolower s2)))))
 
-(defpackage No-case-string-set 
-  (Set-impl No-case-string))
+(defpackage No-case-string-set
+  (Abstract-set No-case-string))
 
 ;; --
 
 (defpackage My
   (implementation ()
-    (let-package S = No-case-string-set
-      (let set = (S::make)
-        (S::add set "foo")))))
+    (import S = No-case-string-set)
+    (let set = (S::make)
+      (S::add set "foo"))))
 
 ;; Now I just need to figure out how to compile this.
+
+;; http://caml.inria.fr/pub/docs/manual-ocaml/manual004.html
