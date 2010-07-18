69b49a480cec463db5444c87b0e4968081b19ae3
Manuel Simoni <msimoni@gmail.com>
2010-07-18 06:15:37 +0200

    defun and friends

--- boot.lisp
+++ boot.lisp
@@ -9,3 +9,37 @@
 
 (defmacro progn (&rest exprs)
   #`(ell-seq ,@exprs))
+
+(defmacro lambda (sig &rest body)
+  #`(ell-lam ,sig (progn ,@body)))
+
+(defmacro if (test then &optional (else #'unspecified))
+  #`(ell-cond ,test ,then ,else))
+
+(defmacro when (test &rest body)
+  #`(if ,test (progn ,@body)))
+
+(defmacro not (x)
+  #`(if ,x #f #t))
+
+(defmacro unless (test &rest body)
+  #`(when (not ,test) ,@body))
+
+(defmacro definedp (name)
+  #`(ell-defp ,name))
+
+(defmacro fdefinedp (name)
+  #`(ell-fdefp ,name))
+
+(defmacro defparameter (name value)
+  #`(ell-def ,name ,value))
+
+(defmacro defvar (name value)
+  #`(unless (definedp ,name)
+      (defparameter ,name ,value)))
+
+(defmacro define (name function)
+  #`(ell-fdef ,name ,function))
+
+(defmacro defun (name sig &rest body)
+  #`(define ,name (lambda ,sig ,@body)))
