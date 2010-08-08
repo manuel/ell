5917b094a3b00f626711bdeeeeb31666afb41528
Manuel Simoni <msimoni@gmail.com>
2010-08-08 02:24:31 +0200

    generics, part 3

---
+++ defgeneric.h
@@ -0,0 +1,13 @@
+/* This x-header is included from `ellrt.c' and `ellrt.h' with
+   different definitions of `ELL_DEFGENERIC(name, lisp_name)'. */
+
+ELL_DEFGENERIC(add, "add")
+ELL_DEFGENERIC(all, "all")
+ELL_DEFGENERIC(emptyp, "emptyp")
+ELL_DEFGENERIC(first, "first")
+ELL_DEFGENERIC(fourth, "fourth")
+ELL_DEFGENERIC(front, "front")
+ELL_DEFGENERIC(popDfront, "pop-front")
+ELL_DEFGENERIC(printDobject, "print-object")
+ELL_DEFGENERIC(second, "second")
+ELL_DEFGENERIC(third, "third")
